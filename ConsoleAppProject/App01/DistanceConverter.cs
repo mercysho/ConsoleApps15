f0b733fec03b0a95559c1296bf83cdecf6859648
Mercy E <22130778@bucks.ac.uk>
2022-02-07 22:16:42 +0000

    entered execute choice

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -2,7 +2,7 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// The main features of this app is to 
     /// </summary>
     /// <author>
     /// Mercy Sholola version 0.1
@@ -19,22 +19,61 @@ namespace ConsoleAppProject.App01
         /// will calculate the same distance in feet, and finally output the distance also in
         /// feet.
         /// </summary>
+
         public void Run()
         { 
-            OutputHeading();  
-          InputMiles();
-          CalculateFeet();
-          OutputFeet();
-        }
-
-        private void OutputHeading()
-        {
             Console.WriteLine("\n-------------------------------------------");
             Console.WriteLine("      Convert Miles to Feet               ");
             Console.WriteLine("         by Mercy Sholola                 ");
             Console.WriteLine("-------------------------------------------\n");
+            int choice = InputChoice();
+            ExcecuteChoice(choice);
+        }
+        
+        /// <summary>
+        /// This method will give the user the options to either convert the value they
+        /// have chosen from miles to feet or feet to miles.
+        /// </summary>
+        private int InputChoice()
+        {
+            Console.WriteLine("Do you want to convert miles to feet or feet to miles?");
+            Console.WriteLine("1.Miles to feet");
+            Console.WriteLine("2.Feet to miles");
+            
+            int value = Console.Read();
+
+            if (value == 1)
+            {
+                return value;
+            }
+            else if (value == 2)
+            {
+                return value;
+            }
+            else
+            {
+                Console.Beep();
+                Console.WriteLine("Please choose either 1 or 2" );
+                return 0;
+            }
         }
 
+        private void ExcecuteChoice(int choice)
+        {
+            if (choice == 1)
+            {
+                InputMiles(); 
+                CalculateFeet(); 
+                OutputFeet();
+            }
+            else if (choice == 2)
+            {
+                InputMiles(); 
+                CalculateFeet(); 
+                OutputFeet();
+            }
+        }
+       
         /// <summary>
         /// Prompt the user to enter distance in miles
         /// Input miles as a double digit
